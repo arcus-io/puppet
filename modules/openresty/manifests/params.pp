c87c1a4304935717a00574a8dc2b3660fd7278c0
Evan Hazlett <ejhazlett@gmail.com>
2012-12-20 01:32:18 -0500

    added luasocket to openresty

--- modules/openresty/manifests/params.pp
+++ modules/openresty/manifests/params.pp
@@ -1,3 +1,4 @@
 class openresty::params {
   $openresty_url = 'http://agentzh.org/misc/nginx/ngx_openresty-1.2.4.13.tar.gz'
+  $luasocket_url = 'http://files.luaforge.net/releases/luasocket/luasocket/luasocket-2.0.2/luasocket-2.0.2.tar.gz'
 }
