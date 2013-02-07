de08b524b84362ca727812c1bd6b687ea9b9482f
Evan Hazlett <ejhazlett@gmail.com>
2013-02-07 12:52:27 -0500

    fixed sentry params

--- modules/sentry/manifests/params.pp
+++ modules/sentry/manifests/params.pp
@@ -5,7 +5,7 @@ class sentry::params {
   $sentry_db_port = hiera('sentry_db_port', '5432')
   $sentry_db_name = hiera('sentry_db_name', 'sentry')
   $sentry_db_user = hiera('sentry_db_user', 'sentry')
-  $sentry_db_password = hiera('sentry_db_user', 's3ntryapp')
+  $sentry_db_password = hiera('sentry_db_password', 's3ntryapp')
   $sentry_url_prefix = hiera('sentry_url_prefix', 'http://sentry.local')
   $sentry_key = hiera('sentry_key', 'abcdefg12345')
   $sentry_ve_dir = hiera('sentry_ve_dir', '/opt/sentry')
