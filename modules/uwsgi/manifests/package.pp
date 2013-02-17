7f454538e23379d7a51d8d4fb08505fbe051dbf5
Evan Hazlett <ejhazlett@gmail.com>
2013-02-17 12:03:49 -0500

    added uwsgi module

---
+++ modules/uwsgi/manifests/package.pp
@@ -0,0 +1,27 @@
+class uwsgi::package inherits uwsgi::params {
+  Exec {
+    path      => "${::path}",
+    logoutput => on_failure,
+  }
+  if ! defined(Package["libxslt-dev"]) { package { "libxslt-dev": ensure => installed, } }
+  if ! defined(Package["libxml2-dev"]) { package { "libxml2-dev": ensure => installed, } }
+  if ! defined(Package["libssl-dev"]) { package { "libssl-dev": ensure => installed, } }
+  if ! defined(Package["supervisor"]) { package { "supervisor": ensure => installed, } }
+  # install pip
+  exec { 'uwsgi::package::install_pip':
+    command   => 'easy_install pip',
+    user      => root,
+    unless    => 'test -e /usr/local/bin/pip',
+  }
+  exec { 'uwsgi::package::install_uwsgi':
+    cwd     => '/tmp',
+    command => 'pip install uwsgi',
+    unless  => 'test -d /usr/local/bin/uwsgi',
+    require  => [ 
+      Package['libxslt-dev'],
+      Package['libxml2-dev'],
+      Package['libssl-dev'],
+      Exec['uwsgi::package::install_pip'],
+    ],
+  }
+}
---
+++ modules/uwsgi/manifests/params.pp
@@ -0,0 +1,3 @@
+class uwsgi::params {
+  $uwsgi_url = 'http://projects.unbit.it/downloads/uwsgi-lts.tar.gz'
+}
