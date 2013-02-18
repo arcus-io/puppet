0d8188836aca57b120fdbf2c5e26a61fc3596355
Evan Hazlett <ejhazlett@gmail.com>
2013-02-18 16:45:28 -0500

    fixed dependency bug with uwsgi

--- modules/uwsgi/manifests/package.pp
+++ modules/uwsgi/manifests/package.pp
@@ -6,12 +6,14 @@ class uwsgi::package inherits uwsgi::params {
   if ! defined(Package["libxslt-dev"]) { package { "libxslt-dev": ensure => installed, } }
   if ! defined(Package["libxml2-dev"]) { package { "libxml2-dev": ensure => installed, } }
   if ! defined(Package["libssl-dev"]) { package { "libssl-dev": ensure => installed, } }
+  if ! defined(Package["python-setuptools"]) { package { "python-setuptools": ensure => installed, } }
   if ! defined(Package["supervisor"]) { package { "supervisor": ensure => installed, } }
   # install pip
   exec { 'uwsgi::package::install_pip':
     command   => 'easy_install pip',
     user      => root,
     unless    => 'test -e /usr/local/bin/pip',
+    require   => Package['python-setuptools'],
   }
   exec { 'uwsgi::package::install_uwsgi':
     cwd     => '/tmp',
