d0702d29d2e0263b38aef04306cc89982fc31217
Evan Hazlett <ejhazlett@gmail.com>
2013-02-18 18:12:16 -0500

    updated mysql

--- modules/mysql/manifests/config.pp
+++ modules/mysql/manifests/config.pp
@@ -11,16 +11,6 @@ class mysql::config inherits mysql::params {
     require     => Package['mysql-server'],
     refreshonly => true,
   }
-  exec { 'mysql::config::create_unisubs_db':
-    command   => "echo \"create database unisubs character set utf8;\" | ${mysql::config::mysql_cmd}",
-    unless    => "echo \"show databases;\" | ${mysql::config::mysql_cmd} | grep unisubs",
-    require   => [ Package['mysql-server'], Exec['mysql::config::set_root_password'] ],
-  }
-  exec { 'mysql::config::permissions_unisubs_db':
-    command   => "echo \"grant all on unisubs.* to root@\'%\' identified by \'${mysql::root_password}\';\" | ${mysql::config::mysql_cmd}",
-    unless    => "echo \"show grants for root@'%';\" | ${mysql::config::mysql_cmd} | grep \"GRANT ALL PRIVILEGES ON \\`unisubs\\`.* TO 'root'@'%'\"",
-    require   => [ Package['mysql-server'], Exec['mysql::config::set_root_password'] ],
-  }
   file { 'mysql::config::mysql_config':
     ensure  => present,
     path    => '/etc/mysql/my.cnf',
