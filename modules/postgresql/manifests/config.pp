e2b599c737d251b4c181671b5b386796d7d6ee6f
Evan Hazlett <ejhazlett@gmail.com>
2013-02-27 09:57:28 -0500

    added user sync script for pgbouncer

--- modules/postgresql/manifests/config.pp
+++ modules/postgresql/manifests/config.pp
@@ -20,6 +20,22 @@ class postgresql::config inherits postgresql::params {
     mode    => 0640,
     content => template('postgresql/pgbouncer.ini.erb'),
   }
+  # this script keeps the pgbouncer user list and postgres user passwords in sync
+  file { '/etc/pgbouncer/sync_pg_users.sh':
+    alias   => 'postgresql::config::sync_pg_users',
+    ensure  => present,
+    owner   => 'postgres',
+    group   => 'postgres',
+    mode    => 0755,
+    content => template('postgresql/sync_pg_users.sh.erb'),
+  }
+  cron { 'postgresql::config::pgbouncer_user_list_sync':
+    command   => '/etc/pgbouncer/sync_pg_users.sh',
+    user      => 'postgres',
+    hour      => '*',
+    minute    => '*',
+    require   => File['postgresql::config::sync_pg_users'],
+  }
   # iptables
   file { '/tmp/.arcus.iptables.rules.postgresql':
     ensure  => present,
