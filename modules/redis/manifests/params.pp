51e63c8526509b9b3e289f4349c4c69d059610a6
Evan Hazlett <ejhazlett@gmail.com>
2013-04-06 23:22:05 -0400

    updated redis to 2.6.12

--- modules/redis/manifests/params.pp
+++ modules/redis/manifests/params.pp
@@ -1,6 +1,6 @@
 class redis::params {
   $iptables_hosts = hiera_array('redis_iptables_hosts', ['0.0.0.0/0'])
-  $redis_url = "http://redis.googlecode.com/files/redis-2.4.15.tar.gz"
+  $redis_url = "http://redis.googlecode.com/files/redis-2.6.12.tar.gz"
   $listen_host = hiera('redis_listen_host', '0.0.0.0')
   $port = hiera('redis_port', '6379')
   $timeout = hiera('redis_timeout', '300')
