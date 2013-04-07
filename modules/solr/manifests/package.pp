1a4b0afd63d23c8c9ed3910562c0add7246a9428
Evan Hazlett <ejhazlett@gmail.com>
2013-04-06 22:02:48 -0400

    updated solr install

--- modules/solr/manifests/package.pp
+++ modules/solr/manifests/package.pp
@@ -10,6 +10,7 @@ class solr::package inherits solr::params {
     cwd     => '/opt',
     command => "wget ${solr::params::solr_url} -O /tmp/solr.tar.gz ; tar zxf /tmp/solr.tar.gz ; mv /opt/*solr* /opt/solr",
     unless  => 'test -d /opt/solr',
+    timeout => 1800,
     require  => [ 
       Package['openjdk-7-jdk'],
       Package['openjdk-7-jre-headless'],
