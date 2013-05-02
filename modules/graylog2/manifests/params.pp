1f01db901da98b7188583fd8316f8d56cce3514f
Evan Hazlett <ejhazlett@gmail.com>
2013-05-02 15:32:18 -0400

    dat syntax

--- modules/graylog2/manifests/params.pp
+++ modules/graylog2/manifests/params.pp
@@ -6,7 +6,7 @@ class graylog2::params {
   $graylog_server_dir = "/opt/graylog_server"
   $graylog_server_url = "https://github.com/downloads/Graylog2/graylog2-server/graylog2-server-0.9.6.tar.gz"
   $graylog_web_url = "https://github.com/downloads/Graylog2/graylog2-web-interface/graylog2-web-interface-0.9.6p1-RC1.tar.gz"
-  $graylog_web_port = hiera('graylog_web_port', '3000)
+  $graylog_web_port = hiera('graylog_web_port', '3000')
   $mongodb_url = "http://fastdl.mongodb.org/linux/mongodb-linux-x86_64-2.0.2.tgz"
   $mongodb_dir = "/opt/mongodb"
   $mongodb_data_dir = "/opt/mongodb_data"
