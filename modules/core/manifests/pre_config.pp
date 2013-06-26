1e359577986ced97c3733c41602a62e8eee05a91
Evan Hazlett <ejhazlett@gmail.com>
2013-06-26 11:04:02 -0400

    fixed typo with hosts.d

--- modules/core/manifests/pre_config.pp
+++ modules/core/manifests/pre_config.pp
@@ -1,6 +1,6 @@
 class core::pre_config inherits core::params {
   if ! defined(File['/etc/hosts.d']) {
-    file { ' /etc/hosts.d':
+    file { '/etc/hosts.d':
       ensure    => directory,
       owner     => root,
       group     => root,
