b16f71aeb97cff09eb752f14e831e8bc99e6907a
Evan Hazlett <ejhazlett@gmail.com>
2013-08-06 13:52:47 -0400

    updated nginx conf

--- modules/nginx/manifests/config.pp
+++ modules/nginx/manifests/config.pp
@@ -5,14 +5,6 @@ class nginx::config inherits nginx::params {
     path      => "${::path}",
     logoutput => on_failure,
   }
-  file { '/etc/nginx/nginx.conf':
-    ensure  => present,
-    content => template('nginx/nginx.conf.erb'),
-    owner   => root,
-    mode    => 0644,
-    require => Package['nginx'],
-    notify  => Service['nginx'],
-  }
   file { '/etc/nginx/sites-available/default':
     ensure  => present,
     content => template('nginx/default.conf.erb'),
